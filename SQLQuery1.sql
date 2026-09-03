aea9a791823f2720f374008fc82ff3f8df476b63
Raphael <74225095+Raphael-Panda@users.noreply.github.com>
2026-09-03 10:51:30 -0300

    Store Procedure

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,31 +1,109 @@
--- PROGRAMAÇÃO EM SQL
-if
-	begin 
+--criação de variavel precisa ter @ antes do nome
+--precisa declarar a variavel antes de usar com declare
+--após o declare vem o nome e o valor da variavel @valor numeric(18,2)
+--para atribruir um valor a uma variavel usa-se set e/ou select
 
-	end
--- begin é a primeira condição do if e o end é casa o begin seja falso, else
--- e o case que funciona parecido com o switch
------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------
 
-select
-	case datepart(dw,getdate())
-	when 1 then 'Domingo'
-	when 2 then 'Segunda'
-	when 3 then 'Terça'
-	when 4 then 'Quarta'
-	when 5 then 'Quinta'
-	when 6 then 'Sexta'
-	when 7 then 'Sabado'
-	end as dia
--- retornar um numero que é equivalente ao dia da semana
------------------------------------------------------------------------------------------------------------------------
+declare @totalgeral numeric(18,2)
 
-select
-	emp_razaosocial,
-	sum(case when pag_datapagto is null then pag_valor else 0 end) as 'valor a pagar',
-	sum(case when pag_datapagto is not null then pag_valor else 0 end) as 'valor ja pago'
-from empresa, pagar
-where FkEmpresa = idempresa
+select @totalgeral = sum(pag_valor)
+from pagar
+where pag_datapagto is null
+
+select emp_razaosocial,
+sum(pag_valor) as totalempresa,
+(sum(pag_valor)/@totalgeral) * 100 as '% do valor em atraso'
+from pagar, empresa 
+
+where fkempresa = idempresa
+and pag_datapagto is null
 group by emp_razaosocial
+having (sum(pag_valor)/@totalgeral) * 100 >= 5
+order by totalempresa desc
 
--- cria duas colunas uma com os valores a pagar e outra com valores ja pagos
+----------------------------------------------------------------------------------------------------------------------------
+
+create proc faturas_pagar
+as
+declare @totalgeral numeric(18,2)
+select @totalgeral = sum(pag_valor)
+from pagar
+where pag_datapagto is null
+
+select emp_razaosocial,
+sum(pag_valor) as totalempresa,
+(sum(pag_valor)/@totalgeral) * 100 as '% do valor em atraso'
+from pagar, empresa 
+
+where fkempresa = idempresa
+and pag_datapagto is null
+group by emp_razaosocial
+having (sum(pag_valor)/@totalgeral) * 100 >= 5
+order by totalempresa desc
+
+exec faturas_pagar
+
+--criando Store Procedure
+
+----------------------------------------------------------------------------------------------------------------------------
+
+alter proc faturas_pagar
+(@datainicial date, @datafinal date)
+as
+declare @totalgeral numeric(18,2)
+select @totalgeral = sum(pag_valor)
+from pagar
+where pag_datapagto is null
+and pag_datavencimento >= @datainicial
+and pag_datavencimento <= @datafinal
+
+select emp_razaosocial,
+sum(pag_valor) as totalempresa,
+(sum(pag_valor)/@totalgeral) * 100 as '% do valor em atraso'
+from pagar, empresa 
+
+where fkempresa = idempresa
+and pag_datapagto is null
+and pag_datavencimento >= @datainicial
+and pag_datavencimento <= @datafinal
+group by emp_razaosocial
+having (sum(pag_valor)/@totalgeral) * 100 >= 5
+order by totalempresa desc
+
+EXEC faturas_pagar @datainicial = '2026-01-01', @datafinal = '2026-12-12';
+
+-- buscando agora as faturas com um filtro de data incial e final dentro do store procedure
+
+----------------------------------------------------------------------------------------------------------------------------
+
+alter proc faturas_pagar
+(@datainicial date, @datafinal date, @empresa varchar(50))
+as
+declare @totalgeral numeric(18,2)
+select @totalgeral = sum(pag_valor)
+from pagar, empresa
+where pag_datapagto is null
+and pag_datavencimento >= @datainicial
+and pag_datavencimento <= @datafinal
+and emp_razaosocial like '%' + @empresa + '%'
+
+select emp_razaosocial,
+sum(pag_valor) as totalempresa,
+(sum(pag_valor)/@totalgeral) * 100 as '% do valor em atraso'
+from pagar, empresa 
+
+where fkempresa = idempresa
+and pag_datapagto is null
+and pag_datavencimento >= @datainicial
+and pag_datavencimento <= @datafinal
+and emp_razaosocial like '%' + @empresa + '%'
+
+group by emp_razaosocial
+having (sum(pag_valor)/@totalgeral) * 100 >= 5
+order by totalempresa desc
+
+EXEC faturas_pagar @datainicial = '2026-01-01', @datafinal = '2026-12-12',
+				   @empresa = 'Atlântica';
+
+--agora que possa ser buscada uma empresa em especifico
